00157a6c9a622e9687f950453d99fa350c2838b6
muhsin <muhsin7ak@gmail.com>
2026-07-24 01:13:20 +0530

    new: better camera & character movement
    - turn smoothly
    - face character to where camera is pointed
    - adjust gravity for jump speed
    - adjust overally movement smoothness

--- scripts/camera_control.gd
+++ scripts/camera_control.gd
@@ -1,8 +1,10 @@
 extends SpringArm3D
 
 @onready var camera: Camera3D = $Camera3D
+@onready var player: CharacterBody3D = self.get_parent()
 var mouse_input: Vector2 = Vector2()
 var mouse_sensitivity := 0.5
+var camera_offset: float = self.position.y
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,6 +21,10 @@ func _process(delta: float) -> void:
 	rotation_degrees.x = clampf(rotation_degrees.x,  -45, 50)
 	
 	mouse_input = Vector2()
+	
+func _physics_process(delta: float) -> void:
+	# Set position of the camera to position of player + the offset to keep it a top down-ish view
+	self.position = player.position + Vector3(0,camera_offset,0)
 	
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
--- scripts/character_body_3d.gd
+++ scripts/character_body_3d.gd
@@ -1,28 +1,43 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const TURN_SPEED = 15
 
+@onready var camera: Camera3D = $SpringArm3D/Camera3D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * delta * 1.5
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get input vector
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
+	
+	# Calculate direction relative to camera transform
+	var direction := camera.global_basis * Vector3(input_dir.x, 0, input_dir.y)
+	
+	# Flatten vector to horizontal plane (keep X and Z, eliminate vertical tilt)
+	direction.y = 0
+	direction = direction.normalized()
+	
+	if direction != Vector3.ZERO:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		
+		turn_mesh(direction, delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		
 	move_and_slide()
+	
+func turn_mesh(direction: Vector3, delta: float) -> void:
+	var yaw = atan2(-direction.x, -direction.z)
+
+	# Lerping to enable smooth turning
+	self.rotation.y = lerp_angle(rotation.y, yaw, TURN_SPEED * delta)
