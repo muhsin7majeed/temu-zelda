c7029a57d19d4ad25a5315efef08700b31a0f5f6
muhsin <muhsin7ak@gmail.com>
2026-07-23 20:42:24 +0530

    init: first commit
    - setup project
    - setup basic scenes
    - setup player & world scenes
    - setup camera & basic movement controls

---
+++ scripts/camera_control.gd
@@ -0,0 +1,32 @@
+extends SpringArm3D
+
+@onready var camera: Camera3D = $Camera3D
+var mouse_input: Vector2 = Vector2()
+var mouse_sensitivity := 0.5
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	spring_length = camera.position.z
+	
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	rotation_degrees.x += mouse_input.y
+	rotation_degrees.y += mouse_input.x
+	
+	rotation_degrees.x = clampf(rotation_degrees.x,  -45, 50)
+	
+	mouse_input = Vector2()
+	
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		mouse_input = -event.relative * mouse_sensitivity
+		
+	elif event is InputEventKey and event.keycode == KEY_ESCAPE and event.is_pressed():
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		
+	elif event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		
